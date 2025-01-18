ea76be0f2b748c4225bdd871481c2444c95283a4
“Denys <denn8051@gmail.com>
2025-01-18 01:45:24 +0100

    first commit

---
+++ src/test/java/com/Musicom/MusicomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musicom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
