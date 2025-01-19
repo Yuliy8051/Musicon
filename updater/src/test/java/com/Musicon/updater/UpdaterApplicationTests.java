cb6d38b972a598580e0d5d39c6f5776416ce8457
“Denys <denn8051@gmail.com>
2025-01-19 17:10:41 +0100

    add updater module

---
+++ updater/src/test/java/com/Musicon/updater/UpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musicon.updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
