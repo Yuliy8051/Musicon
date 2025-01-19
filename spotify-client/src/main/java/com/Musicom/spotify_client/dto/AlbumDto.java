28426cc759d9845e80f7ff286ae0cc236274d003
“Denys <denn8051@gmail.com>
2025-01-19 16:50:18 +0100

    change types of AlbumDto::id and AlbumDto::releaseDate from int and LocalDate to String

--- spotify-client/src/main/java/com/Musicom/spotify_client/dto/AlbumDto.java
+++ spotify-client/src/main/java/com/Musicom/spotify_client/dto/AlbumDto.java
@@ -2,17 +2,16 @@ package com.Musicom.spotify_client.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.time.LocalDate;
 import java.util.List;
 
-public record AlbumDto(int id,
+public record AlbumDto(String id,
                        @JsonProperty("total_tracks")
                        int totalTracks,
                        @JsonProperty("external_urls")
                        UrlDto urlDto,
                        String name,
                        @JsonProperty("release_date")
-                       LocalDate releaseDate,
+                       String releaseDate,
                        List<ImageDto> images,
                        @JsonProperty("album_type")
                        String albumType,
