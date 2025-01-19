189212f375e96f737ae3bd1efb4112b23e366c41
“Denys <denn8051@gmail.com>
2025-01-19 17:23:46 +0100

    add UpdaterConfig

---
+++ updater/src/main/java/com/Musicon/updater/UpdaterConfig.java
@@ -0,0 +1,9 @@
+package com.Musicon.updater;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.Musicom.data", "com.Musicom.spotify_client"})
+public class UpdaterConfig {
+}
