543ff893600166e742ba4722ffff0b78987faa86
“Denys <denn8051@gmail.com>
2025-01-20 09:45:42 +0100

    add config class for test

---
+++ test/src/main/java/com/Musicom/test/Config.java
@@ -0,0 +1,9 @@
+package com.Musicom.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.Musicom.updater"})
+public class Config {
+}
