0ac0e4bc27b953d1a4c4d257f4a288d624567bd7
“Denys <denn8051@gmail.com>
2025-01-22 17:18:11 +0100

    add dependency on data module to api module

--- api/src/main/java/com/Musicom/api/ApiConfiguration.java
+++ api/src/main/java/com/Musicom/api/ApiConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"com.Musicom.updater"})
+@ComponentScan({"com.Musicom.updater", "com.Musicom.data"})
 public class ApiConfiguration {
 }
