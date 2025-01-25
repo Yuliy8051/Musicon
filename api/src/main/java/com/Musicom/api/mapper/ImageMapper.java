99cf730030bfd586b7c0acd80d53843ad3db0169
“Denys <denn8051@gmail.com>
2025-01-25 14:56:03 +0100

    [api] add ImageMapper,

---
+++ api/src/main/java/com/Musicom/api/mapper/ImageMapper.java
@@ -0,0 +1,33 @@
+package com.Musicom.api.mapper;
+
+import com.Musicom.data.model.Image;
+import com.Musicom.web_api_contract.ImageDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component("apiImageMapper")
+public class ImageMapper implements IMap<ImageDto, Image> {
+    @Override
+    public List<ImageDto> mapAllEntities(List<Image> images) {
+        return images
+                .stream()
+                .map(this::mapEntity)
+                .toList();
+    }
+
+    @Override
+    public Image mapDto(ImageDto imageDto) { // TODO
+        return null;
+    }
+
+    @Override
+    public ImageDto mapEntity(Image image) {
+        ImageDto imageDto = new ImageDto();
+        imageDto.setId(image.getId());
+        imageDto.setUrl(image.getUrl());
+        imageDto.setHeight(image.getHeight());
+        imageDto.setWidth(image.getWidth());
+        return imageDto;
+    }
+}
