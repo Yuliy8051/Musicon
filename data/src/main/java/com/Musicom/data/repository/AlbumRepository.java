6063a7db5a63659a9428c36d33bea35f85ccbfc4
“Denys <denn8051@gmail.com>
2025-01-25 14:45:40 +0100

    [data] add AlbumRepository::countAll, AlbumRepository::findPage

--- data/src/main/java/com/Musicom/data/repository/AlbumRepository.java
+++ data/src/main/java/com/Musicom/data/repository/AlbumRepository.java
@@ -2,7 +2,17 @@ package com.Musicom.data.repository;
 
 import com.Musicom.data.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     Album findBySourceId(String sourceId);
+
+    @Query("select count(a) from Album a")
+    Long countAll();
+
+    @Query(value = "select * from albums limit :limit offset :offset", nativeQuery = true)
+    List<Album> findPage(@Param("offset") long offset, @Param("limit") int limit);
 }
