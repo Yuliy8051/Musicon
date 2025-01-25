1cf7eda24a779363f8e5060919e8c715f090e08a
“Denys <denn8051@gmail.com>
2025-01-25 18:05:00 +0100

    [data] add BandRepository::countAll, BandRepository::findPage

--- data/src/main/java/com/Musicom/data/repository/BandRepository.java
+++ data/src/main/java/com/Musicom/data/repository/BandRepository.java
@@ -2,6 +2,8 @@ package com.Musicom.data.repository;
 
 import com.Musicom.data.model.Band;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Set;
@@ -9,4 +11,10 @@ import java.util.Set;
 public interface BandRepository extends JpaRepository<Band, Long> {
     Band findBySourceId(String sourceId);
     Set<Band> findBySourceIdIn(List<String> sourceIds);
+
+    @Query(value = "select count(b) from Band b")
+    Long countAll();
+
+    @Query(value = "select * from tracks limit :limit offset :offset", nativeQuery = true)
+    List<Band> findPage(@Param("offset") long offset, @Param("limit") int limit);
 }
